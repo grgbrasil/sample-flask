fd412e15126c9d885a54a0d4f9f980682e7e60e0
Gabriel <grgbrasil@gmail.com>
2021-05-07 12:12:51 -0300

    firts

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ def pdftocsv(request):
         f = request.files['file']
         arquivo = tabula.read_pdf(f,pages='all')
         tabula.convert_into(f, 'seuarquivo.csv', output_format='csv', pages='all')
-        f.save(secure_filename(f.filename))
+        # f.save(secure_filename(f.filename))
         return 'file uploaded successfully'
