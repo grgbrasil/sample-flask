a7693520a575e3f75bb023aec21bfc68667316d0
Gabriel Rodrigues Garcia <grgbrasil@gmail.com>
2021-05-07 10:49:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,13 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route("/pdftocsv")
+def pdftocsv(request):
+   if request.method == 'POST':
+      f = request.files['file']
+      arquivo = tabula.read_pdf(f,pages='all')
+      tabula.convert_into(f, 'seuarquivo.csv', output_format='csv', pages='all')
+
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
