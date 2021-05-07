93bebec82dd9d8fc9e0fc9d10a143eaa2e2a5aee
Gabriel Rodrigues Garcia <grgbrasil@gmail.com>
2021-05-07 11:02:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 from flask import Flask
 from flask import render_template
+import tabula
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
 
-@app.route("/pdftocsv")
+@app.route('/pdftocsv', methods=['POST', 'GET'])
 def pdftocsv(request):
-   if request.method == 'POST':
-      f = request.files['file']
-      arquivo = tabula.read_pdf(f,pages='all')
-      tabula.convert_into(f, 'seuarquivo.csv', output_format='csv', pages='all')
-
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+    if request.method == 'POST':
+        f = request.files['file']
+        arquivo = tabula.read_pdf(f,pages='all')
+        tabula.convert_into(f, 'seuarquivo.csv', output_format='csv', pages='all')
+        f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
