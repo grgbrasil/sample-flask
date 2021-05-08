8f854f785ee004fe79ab28435c5713a00c721934
Gabriel Rodrigues Garcia <grgbrasil@gmail.com>
2021-05-08 12:58:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
-from flask import Flask
-from flask import render_template
-import tabula
+import os
+from flask import Flask, render_template, request, redirect, url_for, abort
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+app.config['UPLOAD_PATH'] = 'uploads'
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/pdftocsv', methods=['POST', 'GET'])
-def pdftocsv(request):
-    if request.method == 'POST':
-        f = request.files['file']
-        arquivo = tabula.read_pdf(f,pages='all')
-        tabula.convert_into(f, 'seuarquivo.csv', output_format='csv', pages='all')
-        # f.save(secure_filename(f.filename))
-        return 'file uploaded successfully'
+@app.route('/', methods=['POST'])
+def upload_files():
+    uploaded_file = request.files['file']
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            abort(400)
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
+    return redirect(url_for('index'))
